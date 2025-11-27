b0aa32b692855ebd650cb37b67647ce3c66702fb
hyunjo01 <hyunjo10217@gmail.com>
2025-11-27 16:13:17 +0900

    [test] - article 조회 cache hit 테스트

--- api-server/src/test/java/com/ssafy/newstagram/api/article/service/ArticleServiceTest.java
+++ api-server/src/test/java/com/ssafy/newstagram/api/article/service/ArticleServiceTest.java
@@ -1,6 +1,7 @@
 package com.ssafy.newstagram.api.article.service;
 
 import com.ssafy.newstagram.api.article.dto.ArticleDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,19 +18,31 @@ public class ArticleServiceTest {
     @Autowired
     private ArticleService articleService;
 
+    private String key = "article:123";
+    private Long id = 123L;
+
+    @BeforeEach
+    void init() {
+        redisTemplate.delete(key);
+    }
+
     @Test
-    void test() {
+    void hitCache() {
         ArticleDto articleDto = ArticleDto.builder()
-                .id(123L).author("testAuthor").title("testTitle").build();
-        redisTemplate.opsForValue().set("testArticleDtoKey", articleDto);
+                .id(id).author("testAuthor").title("testTitle").build();
+        redisTemplate.opsForValue().set(key, articleDto);
 
-        ArticleDto result = (ArticleDto) redisTemplate.opsForValue().get("testArticleDtoKey");
-
-//        ArticleDto result = articleService.getArticleDto(123L);
+        ArticleDto result = (ArticleDto) redisTemplate.opsForValue().get(key);
         assertNotNull(result);
         assertEquals(articleDto.getId(), result.getId());
         assertEquals(articleDto.getAuthor(), result.getAuthor());
         assertEquals(articleDto.getTitle(), result.getTitle());
+
+        ArticleDto serviceResult = articleService.getArticleDto(id);
+        assertNotNull(serviceResult);
+        assertEquals(articleDto.getId(), serviceResult.getId());
+        assertEquals(articleDto.getAuthor(), serviceResult.getAuthor());
+        assertEquals(articleDto.getTitle(), serviceResult.getTitle());
     }
 
 }
