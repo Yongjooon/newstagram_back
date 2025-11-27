2204d250a7c5927e09626d184bc9e7ebedc6ae4a
hyunjo01 <hyunjo10217@gmail.com>
2025-11-27 16:08:38 +0900

    [refactor] - article entity 삭제 (공통 모듈 엔티티 사용)

--- api-server/src/main/java/com/ssafy/newstagram/api/article/repository/ArticleRepository.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/article/repository/ArticleRepository.java
@@ -1,6 +1,6 @@
 package com.ssafy.newstagram.api.article.repository;
 
-import com.ssafy.newstagram.api.article.entity.Article;
+import com.ssafy.newstagram.domain.news.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
