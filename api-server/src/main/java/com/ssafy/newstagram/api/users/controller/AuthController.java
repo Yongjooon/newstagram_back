136542260c9772bfd1cfc86c055ab39934137cf9
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-02 17:02:35 +0900

    [feat] - AuthController에login api 추가

--- api-server/src/main/java/com/ssafy/newstagram/api/users/controller/AuthController.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/users/controller/AuthController.java
@@ -1,4 +1,37 @@
 package com.ssafy.newstagram.api.users.controller;
 
+import com.ssafy.newstagram.api.auth.model.dto.LoginRequestDto;
+import com.ssafy.newstagram.api.auth.model.dto.LoginResponseDto;
+import com.ssafy.newstagram.api.users.model.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "AuthController", description = "인증/인가를 위한 기능 제공")
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    @Operation(summary = "일반 로그인", description = "이베일 기반으로 일반 로그인을 요청한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공")
+    })
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto dto){
+        // 로그인 실패 시에는, ExceptionHandler가 에러 응답 생성
+        LoginResponseDto response = userService.login(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
