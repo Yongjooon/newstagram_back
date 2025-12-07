4061af1ab72ada00e0fab2a0d22646833b727dba
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-08 04:24:59 +0900

    [feat] - CORS 설정

--- api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ssafy.newstagram.api.auth.jwt.JWTFilter;
 import com.ssafy.newstagram.api.auth.jwt.JWTUtil;
 import com.ssafy.newstagram.api.auth.jwt.LoginFilter;
 import com.ssafy.newstagram.api.auth.model.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -49,6 +54,25 @@ public class SecurityConfig {
         // LoginFilter 생성 + URL 설정
         LoginFilter loginFilter = new LoginFilter(authManager, jwtUtil);
         loginFilter.setFilterProcessesUrl("/auth/login"); // 로그인 URL
+
+        // CORS 설정
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        return configuration;
+                    }
+                }));
 
         http
             .authorizeHttpRequests(auth -> auth
