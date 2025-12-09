443c9c09b679c157b790e81bd087693cfeb2fa64
dabinchiii <81626630+dabinchiii@users.noreply.github.com>
2025-12-09 14:40:10 +0900

    [feat] - 순환 참조 문제를 해결하기 위해 passwordEncoder를 개별 클래스로 분리

--- api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/config/SecurityConfig.java
@@ -10,12 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -36,11 +34,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCrybpPasswordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
