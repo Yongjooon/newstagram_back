d5d5892011fdcb96498863e5f4b8a86a9b0286ca
hyunjo01 <hyunjo10217@gmail.com>
2025-12-17 17:56:47 +0900

    [feat] - ArticleController: 페이징 처리한 hotIssue 호출 메서드

--- api-server/src/main/java/com/ssafy/newstagram/api/article/controller/ArticleController.java
+++ api-server/src/main/java/com/ssafy/newstagram/api/article/controller/ArticleController.java
@@ -1,7 +1,7 @@
 package com.ssafy.newstagram.api.article.controller;
 
 import com.ssafy.newstagram.api.article.dto.ArticleDto;
-import com.ssafy.newstagram.api.article.dto.HotIssueSetDto;
+import com.ssafy.newstagram.api.article.dto.HotIssueResponse;
 import com.ssafy.newstagram.api.article.service.ArticleService;
 import com.ssafy.newstagram.api.article.service.HotIssueService;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +16,8 @@ public class ArticleController {
 
     private final ArticleService articleService;
     private final HotIssueService hotIssueService;
+    private static final int GROUP_SIZE = 10;
+    private static final int LIMIT_COUNT = 5;
 
 
     @GetMapping("/detail/{id}")
@@ -23,15 +25,27 @@ public class ArticleController {
         return articleService.getArticleDto(id);
     }
 
-    @GetMapping("/hot-issues")
-    public ResponseEntity<HotIssueSetDto> getHotIssues(
-            @RequestParam(name = "periodType") String periodType,
-            @RequestParam(name = "limitCount", defaultValue = "3") int limitCount
+//    @GetMapping("/hot-issues")
+//    public ResponseEntity<HotIssueSetDto> getHotIssues(
+//            @RequestParam(name = "periodType") String periodType,
+//            @RequestParam(name = "limitCount", defaultValue = LIMIT_COUNT) int limitCount
+//    ) {
+//        return ResponseEntity.ok(
+//                hotIssueService.getPeriodRecommendationsTopNPerRankingByPeriodType(
+//                        periodType,
+//                        limitCount
+//                )
+//        );
+//    }
+
+    @GetMapping("/hot-issues/{periodType}/{cursor}")
+    public ResponseEntity<HotIssueResponse> getHotIssueArticleRandomly(
+            @PathVariable(name = "periodType") String periodType,
+            @PathVariable(name = "cursor") Integer cursor
     ) {
         return ResponseEntity.ok(
-                hotIssueService.getPeriodRecommendationsTopNPerRankingByPeriodType(
-                        periodType,
-                        limitCount
+                hotIssueService.getHotIssueResponse(
+                        periodType, GROUP_SIZE, LIMIT_COUNT, cursor
                 )
         );
     }
